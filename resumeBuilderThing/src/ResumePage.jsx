3cac2e527ebac723ef7a927cf9cd5cea53e7b6e1
mahogany00 <yashuwuxd@gmail.com>
2025-08-20 19:24:50 +0530

    gotten a bit far with it still way more work to do

--- resumeBuilderThing/src/ResumePage.jsx
+++ resumeBuilderThing/src/ResumePage.jsx
@@ -7,9 +7,33 @@ export default function ResumePage(){
         <div className="encloser">
             <div className="page">
             <div className="topHalf">
-                <h1 className="name">Name here</h1>
+                <h1 className="name">N a m e</h1>
+                <h3 className="jobTitle">Where you work and your title</h3>
             </div>
-            </div>
+
+                <div className="subsection1">
+
+                    <div className="contactSection">
+                    <h3>C o n t a c t</h3>
+                    <div className="list">
+                    <ul>
+                    <li>number</li>
+                    <li>email</li>
+                    <li>address</li>
+                    <li>website</li>
+                    </ul>
+                    </div>
+                    </div>
+                    {/* left half of the bottom part of the page */}
+                </div>
+                <div className="subsection2">
+                    {/* ya guessed it */}
+                </div>
+
+                {/* closing tag for page */}
+
+            </div>  
+            
         </div>
         <textarea name="inputbox" id="inputbox"></textarea>
         </>
